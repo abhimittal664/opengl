527bcb5f9f153936bceb48497f494f137eaa3a46
abhimittal664 <abhimittal664@gmail.com>
2018-11-14 22:10:54 -0800

    Delete init_utils_header.h

--- init_utils_header.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-std::string readShaderFile(const char*);
-std::tuple<GLuint, GLuint> compile_shader(const char*, const char*);
-GLuint createShaderProgram(GLuint, GLuint);
-void populate_vertex_buffer(vertex*, int);
-void draw_geometry(GLuint);
